2470d9491ae046d88d48a5c03570056e12ee8862
Huang Qi <huangqi3@xiaomi.com>
2024-08-22 15:58:00 +0800

    Introduce register operations API for Wasm app
    This patch adds new APIs for Wasm app to operate the register,
    including register read and write operations:
    - putreg8
    - putreg16
    - getreg8
    - getreg16
    - putreg32
    - getreg32
    
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

--- memory/memory.c
+++ memory/memory.c
@@ -16,10 +16,23 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <sys/param.h>
 #include <syslog.h>
 
-#define REGISTER_REGION_START CONFIG_WASM_MEMORY_REGISTER_REGION_START
-#define REGISTER_REGION_END CONFIG_WASM_MEMORY_REGISTER_REGION_END
+#include "wasm_export.h"
+
+#ifdef CONFIG_ARCH_CHIP_BL616
+
+#define REGISTER_REGION_START 0x20000000
+#define REGISTER_REGION_END 0x20072000
+
+#else
+
+#error "No register region defined for this chip"
+
+#endif
+
+#ifdef CONFIG_WASM_MEMORY_REGISTER_ACCESS
 
 /* Forward declarations of WASMModuleInstance struct that only used once in this
  * file
@@ -47,3 +60,59 @@ uint8_t* aot_bounds_check(struct WASMModuleInstance* module_inst, size_t offset,
 
     return (uint8_t*)offset;
 }
+#endif
+
+#ifdef CONFIG_WASM_MEMORY_PUTREG_GETREG
+
+/* Provide the register access functions for WASM */
+
+static void putreg8_wrapper(wasm_exec_env_t exec_env, uint8_t value, uint32_t addr)
+{
+    *((volatile uint8_t*)addr) = value;
+}
+
+static void putreg16_wrapper(wasm_exec_env_t exec_env, uint16_t value, uint32_t addr)
+{
+    *((volatile uint16_t*)addr) = value;
+}
+
+static void putreg32_wrapper(wasm_exec_env_t exec_env, uint32_t value, uint32_t addr)
+{
+    *((volatile uint32_t*)addr) = value;
+}
+
+static uint8_t getreg8_wrapper(wasm_exec_env_t exec_env, uint32_t addr)
+{
+    return *((volatile uint8_t*)addr);
+}
+
+static uint16_t getreg16_wrapper(wasm_exec_env_t exec_env, uint32_t addr)
+{
+    return *((volatile uint16_t*)addr);
+}
+
+static uint32_t getreg32_wrapper(wasm_exec_env_t exec_env, uint32_t addr)
+{
+    return *((volatile uint32_t*)addr);
+}
+
+/* Register access functions */
+
+static NativeSymbol g_register_access_symbols[] = {
+    EXPORT_WASM_API_WITH_SIG2(getreg8, "(i)i"),
+    EXPORT_WASM_API_WITH_SIG2(getreg16, "(i)i"),
+    EXPORT_WASM_API_WITH_SIG2(getreg32, "(i)i"),
+    EXPORT_WASM_API_WITH_SIG2(putreg8, "(ii)"),
+    EXPORT_WASM_API_WITH_SIG2(putreg16, "(ii)"),
+    EXPORT_WASM_API_WITH_SIG2(putreg32, "(ii)"),
+};
+
+/* Register access module, this function is called by the WAMR runtime */
+
+bool wamr_module_register_access_register(void)
+{
+    return wasm_runtime_register_natives(
+        "env", g_register_access_symbols, nitems(g_register_access_symbols));
+}
+
+#endif
