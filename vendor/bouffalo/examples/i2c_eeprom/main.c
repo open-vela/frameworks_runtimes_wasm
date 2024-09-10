463d5dea72743c530427751ea177dd55518decde
wenlingyun1 <wenlingyun1@xiaomi.com>
2024-09-10 09:23:23 +0800

    vendor/bouffalo: Add new example for i2c_10_bit, i2c_eeprom
    Signed-off-by: wenlingyun1 <wenlingyun1@xiaomi.com>

---
+++ vendor/bouffalo/examples/i2c_eeprom/main.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2024 Xiaomi Corperation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "bflb_gpio.h"
+
+void board_init(void) { }
+
+void board_i2c0_gpio_init()
+{
+    struct bflb_device_s* gpio;
+
+    gpio = bflb_device_get_by_name("gpio");
+    /* I2C0_SDA */
+    bflb_gpio_init(gpio, GPIO_PIN_11, GPIO_FUNC_I2C0 | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    /* I2C0_SCL */
+    bflb_gpio_init(gpio, GPIO_PIN_14, GPIO_FUNC_I2C0 | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+}
+
+#include "../../bouffalo_sdk/examples/peripherals/i2c/i2c_eeprom/main.c"
